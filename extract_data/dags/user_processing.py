3bec483c9d7898e09b776d318b0c7dbd5a651d70
Milton Andres Sanabria Cespedes <miltonandres.sanabria.contractor@bbva.com>
2021-12-28 02:21:18 -0500

    all tasks added

--- extract_data/dags/user_processing.py
+++ extract_data/dags/user_processing.py
@@ -1,11 +1,33 @@
 from airflow.models import DAG
-from datetime import datetime
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+
+from datetime import datetime
+import json
+from pandas import json_normalize
 
 
 default_args = {
     'start_date': datetime(2020, 1, 1)
 }
+
+def _processing_user(ti):
+    users = ti.xcom_pull(task_ids=['extracting_user'])
+    if not len(users) or 'results' not in users[0]:
+        raise ValueError('User is empty')
+    user = users[0]['results'][0]
+    processed_user = json_normalize({
+        'firstname': user['name']['first'],
+        'lastname': user['name']['last'],
+        'country': user['location']['country'],
+        'username': user['login']['username'], 
+        'password': user['login']['password'], 
+        'email': user['email']      
+    })
+    processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
 
 with DAG('user_processing',
         default_args = default_args,
@@ -17,7 +39,7 @@ with DAG('user_processing',
         task_id = 'creating_table',
         sqlite_conn_id = 'db_sqlite',
         sql = '''
-            CREATE TABLE users (
+            CREATE TABLE IF NOT EXISTS users (
                 firstname TEXT NOT NULL,
                 lastname TEXT NOT NULL,
                 country TEXT NOT NULL,
@@ -28,4 +50,29 @@ with DAG('user_processing',
         '''
     )
 
-creating_table
+    is_api_available = HttpSensor(
+        task_id = 'is_api_available',
+        http_conn_id = 'user_api',
+        endpoint = 'api/'
+    )
+
+    extracting_user = SimpleHttpOperator(
+        task_id = 'extracting_user',
+        http_conn_id = 'user_api',
+        endpoint = 'api/',
+        method = 'GET',
+        response_filter = lambda response: json.loads(response.text),
+        log_response = True
+    )
+
+    processing_user = PythonOperator(
+        task_id = "processing_user",
+        python_callable = _processing_user
+    )
+
+    storing_user = BashOperator(
+        task_id = "storing_user",
+        bash_command = 'echo -e ".separator ", "\n.import /tmp/processed_user.csv users" | sqlite3 C:\\Users\\LENOVO\\Documents\\apache_airflow\\Airflow_projects\\extract_data\\airflow.db'
+    )
+
+    creating_table >> is_api_available >> extracting_user >> processing_user >> storing_user
